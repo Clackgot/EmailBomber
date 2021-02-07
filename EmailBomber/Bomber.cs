e94f648a6fc4f650b64a91439df0e520a5c74a6c
clackgot <clackgot@gmail.com>
2021-02-08 00:14:19 +0300

    Добавьте файлы проекта.

---
+++ EmailBomber/Bomber.cs
@@ -0,0 +1,29 @@
+﻿using AngleSharp.Dom;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailBomber
+{
+    interface IEmailSendService
+    {
+        Task<IDocument> SendEmail();
+    }
+    class Bomber
+    {
+        public string Email { get; set; }
+        List<IEmailSendService> emailSendsServices = new List<IEmailSendService>();
+        public Bomber(string email)
+        {
+            Email = email;
+        }
+
+        public async Task Bomb()
+        {
+            var emailSendTasks = emailSendsServices.ConvertAll(m => m.SendEmail());
+            await Task.WhenAll(emailSendTasks);
+        }
+
+    }
+}
---
+++ EmailBomber/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmailBomber
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bomber bomber = new Bomber("kavkazkoe@yandex.ru");
+            bomber.Bomb().Wait();
+        }
+    }
+}
